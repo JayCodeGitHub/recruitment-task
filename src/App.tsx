ef010e45752ec47753e5b37ad1b9e7333e279b03
JayCode Developer <JayCode.contakt@gmail.com>
2024-03-28 15:27:18 +0100

    Setup Material UI Library

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from '@mui/material';
 
 function App() {
   return (
     <main>
       <h1>React App</h1>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </main>
   );
 }
