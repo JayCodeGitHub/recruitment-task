72f9787e1b6805b35be5a2c3f33da7dbde22026f
JayCode Developer <JayCode.contakt@gmail.com>
2024-03-28 16:49:21 +0100

    Style App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Button from './components/Button';
+import { Container } from '@material-ui/core';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
-    <main>
-      <h1>React App</h1>
+    <Container maxWidth="sm">
+      <Typography variant="h1" sx={{ color: '#fff', fontSize: '2rem', padding: '1rem 0.5rem' }}>
+        StackOverflow Tags
+      </Typography>
       <Button>Hello World</Button>
       <Button disabled>Hello World</Button>
-    </main>
+    </Container>
   );
 }
 
