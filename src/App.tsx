86fca6fb0d492b7813f634d331d16e35963006c2
JayCode Developer <JayCode.contakt@gmail.com>
2024-03-28 22:03:21 +0100

    Integrate Table Items with global state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Container } from '@material-ui/core';
 import { Typography, Box, CircularProgress } from '@mui/material';
 import Button from './components/Button';
@@ -10,14 +10,13 @@ import axios from 'axios';
 const tableHead = [{ name: 'Tags' }, { name: 'Amount' }];
 
 function App() {
-  const { count } = useStore();
-  const [tableBody, setTableBody] = useState<false | { name: string; count: number }[]>(false);
+  const { items, setItems } = useStore();
 
   const fetchData = async () => {
     const res = await axios.get(
       'https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow'
     );
-    setTableBody(
+    setItems(
       res.data.items.map((item: { name: string; count: number }) => {
         return { name: item.name, count: item.count };
       })
@@ -27,6 +26,7 @@ function App() {
   useEffect(() => {
     fetchData();
   }, []);
+
   return (
     <Container maxWidth="sm">
       <Typography variant="h1" sx={{ color: '#fff', fontSize: '2rem', margin: '1rem 0.5rem' }}>
@@ -35,9 +35,8 @@ function App() {
       <Select />
       <Button>Hello World</Button>
       <Button disabled>Hello World</Button>
-      {count}
-      {tableBody ? (
-        <Table head={tableHead} body={tableBody} />
+      {items ? (
+        <Table head={tableHead} body={items} />
       ) : (
         <Box sx={{ display: 'flex' }}>
           <CircularProgress />
--- src/state/state.ts
+++ src/state/state.ts
@@ -1,17 +1,13 @@
 import { create } from 'zustand';
 
 interface State {
-  count: number;
-  increment: () => void;
-  decrement: () => void;
-  updateState: (newState: number) => void;
+  items: false | { name: string; count: number }[];
+  setItems: (items: false | { name: string; count: number }[]) => void;
 }
 
 const useStore = create<State>((set) => ({
-  count: 0,
-  increment: () => set((prevState: State) => ({ count: prevState.count + 1 })),
-  decrement: () => set((prevState: State) => ({ count: prevState.count - 1 })),
-  updateState: (newState: number) => set({ count: newState })
+  items: false,
+  setItems: (items: false | { name: string; count: number }[]) => set({ items })
 }));
 
 export default useStore;
